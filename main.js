fb78d5c832bb6916b65781b935dcd0eca8663f47
SophiaA1221 <74421045+SophiaA1221@users.noreply.github.com>
2021-03-15 13:48:50 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+console.log("My 1st Console Project");
+function fullName() {
+    var first_name = ["Sophia"];
+    console.log(first_name);
+    var last_name = ["Agboiyi"];
+    console.log(last_name);
+    var full_name = first_name.concat(last_name);
+    console.log(full_name);
+}
